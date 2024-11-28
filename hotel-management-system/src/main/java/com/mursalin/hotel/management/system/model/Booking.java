dd9fc4a227dd9f59a339be7f3e81ff3d50658ef5
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-28 23:52:03 +0600

    user entity upadated and booking entity added

---
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/Booking.java
@@ -0,0 +1,26 @@
+package com.mursalin.hotel.management.system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+}
--- hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/User.java
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/User.java
@@ -1,9 +1,13 @@
 package com.mursalin.hotel.management.system.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -15,9 +19,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "name is required")
     private String name;
+
+    @NotBlank(message = "email is required")
+    @Column(unique = true)
     private String email;
+
+    @NotBlank(message = "phoneNumber is required")
     private String phoneNumber;
     private String password;
     private String role;
+
+    private List<Booking> bookings = new ArrayList<>();
 }
