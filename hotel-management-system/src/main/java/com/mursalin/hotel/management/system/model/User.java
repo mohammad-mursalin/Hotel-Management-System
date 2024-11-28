b0bb5c57ba4fe679e1371b4d71aeaa803965c69c
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-28 23:05:35 +0600

    user entity

---
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/User.java
@@ -0,0 +1,23 @@
+package com.mursalin.hotel.management.system.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+}
