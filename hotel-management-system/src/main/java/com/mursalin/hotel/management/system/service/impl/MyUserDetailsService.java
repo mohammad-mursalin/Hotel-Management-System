634f7df69bfc628f356dc863af1e68abb2dc054b
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-29 23:51:00 +0600

    Custom UserDetailsService added

---
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/service/impl/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.mursalin.hotel.management.system.service.impl;
+
+import com.mursalin.hotel.management.system.exception.OurException;
+import com.mursalin.hotel.management.system.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepo;
+
+    public MyUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findByEmail(username).orElseThrow(()-> new OurException("username or email nor found")) ;
+    }
+}
