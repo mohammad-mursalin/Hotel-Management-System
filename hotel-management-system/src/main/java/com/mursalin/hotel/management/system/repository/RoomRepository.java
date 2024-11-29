4f9caec504a121c000ad9d7ba19080a82e84d9ea
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-29 23:44:10 +0600

    new method added

--- hotel-management-system/src/main/java/com/mursalin/hotel/management/system/repository/RoomRepository.java
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/repository/RoomRepository.java
@@ -16,5 +16,8 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 
     @Query("select r from Room r where r.roomType like %:roomType% and r.id not in " +
             "(select b.room.id from Booking b where b.checkInDate <= :checkOutDate and b.checkOutDate >= :checkInDate)")
-    List<Room> getAllAvailableRooms(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+    List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
+    List<Room> getAllAvailableRooms();
 }
