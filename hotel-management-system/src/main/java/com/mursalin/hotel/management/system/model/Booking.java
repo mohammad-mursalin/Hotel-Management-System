3d6b2100917dddca157428c8f2430539d2816769
mohammad-mursalin <mursalinlamon@gmail.com>
2024-11-29 11:38:04 +0600

    necessary entity classes

--- hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/Booking.java
+++ hotel-management-system/src/main/java/com/mursalin/hotel/management/system/model/Booking.java
@@ -1,6 +1,9 @@
 package com.mursalin.hotel.management.system.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,10 +20,51 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull(message = "check in date is required")
     private LocalDate checkInDate;
+
+    @Future(message = "check out date must be in the future")
     private LocalDate checkOutDate;
+
+    @Min(value = 1, message = "at least one adult is required")
     private int numOfAdults;
+
+    @Min(value = 0, message = "number of children can not be less than 0")
     private int numOfChildren;
     private int totalNumOfGuest;
     private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Room room;
+
+    public void calculateTotalNumOfGuest() {
+        this.totalNumOfGuest = numOfAdults + numOfChildren;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuest();
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuest();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", totalNumOfGuest=" + totalNumOfGuest +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                '}';
+    }
 }
